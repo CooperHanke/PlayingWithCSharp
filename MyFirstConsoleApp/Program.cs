3656723ef0605b7a8c82e801d93f14fe82fc5d24
Cooper Hanke <cooper.hanke@gmail.com>
2018-06-05 12:00:16 -0500

    Testing out using git through VS 2017; init commit

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+    // Applications have a namespace in C#; maybe this is the same thing as a package?
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
